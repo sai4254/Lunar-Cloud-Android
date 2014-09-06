dec36b1199f73b9c1be3616ab46937eb44964367
adityareddyp <adityareddyp@192.168.0.100>
2014-09-07 01:50:21 +0530

    Added User Model Added Preferences Activity

---
+++ Weber/src/in/websock/weber/UserModel.java
@@ -0,0 +1,125 @@
+package in.websock.weber;
+
+public class UserModel {
+
+	
+	private String id;
+    private String guid;
+    private String isActive;
+    private String balance;
+    private String picture;
+    private String age;
+    private String name;
+    private String gender;
+    private String company;
+    private String email;
+    private String phone;
+    private String address;
+    private String registered;
+    private String latitude;
+    private String longitude;
+    private String greeting;
+    private String favoriteFruit;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getGuid() {
+		return guid;
+	}
+	public void setGuid(String guid) {
+		this.guid = guid;
+	}
+	public String getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	public String getBalance() {
+		return balance;
+	}
+	public void setBalance(String balance) {
+		this.balance = balance;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getRegistered() {
+		return registered;
+	}
+	public void setRegistered(String registered) {
+		this.registered = registered;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getGreeting() {
+		return greeting;
+	}
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+	public String getFavoriteFruit() {
+		return favoriteFruit;
+	}
+	public void setFavoriteFruit(String favoriteFruit) {
+		this.favoriteFruit = favoriteFruit;
+	}
+}
